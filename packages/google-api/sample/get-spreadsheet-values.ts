e15c93cd30e94fca5dfa7c991b3d6033abd178e3
genta <code.mizumoto@gmail.com>
2023-09-16 20:35:31 +0900

    spreadsheetの全データを取得する

--- packages/google-api/sample/get-spreadsheet-values.ts
+++ packages/google-api/sample/get-spreadsheet-values.ts
@@ -3,13 +3,25 @@ import { GoogleAPI } from '../src';
 // [usage]
 // npx ts-node sample/get-spreadsheet-values.ts {spreadsheetId} {sheetName}
 
+const spreadsheetId = process.argv[2];
+const sheetName = process.argv[3];
+
 (async () => {
-  const googleApi = new GoogleAPI('mznpm-sample');
+  {
+    const googleApi = new GoogleAPI('mznpm-sample');
+    const values = await googleApi.spreadsheet.getSheetValues({ spreadsheetId: spreadsheetId, range: sheetName });
+    console.log(values);
+  }
 
-  const spreadsheetId = process.argv[2];
-  const sheetName = process.argv[3];
+  {
+    const googleApi = new GoogleAPI('mznpm-sample');
+    const spreadsheetId = process.argv[2];
 
-  const values = await googleApi.spreadsheet.getSheetValues({ spreadsheetId: spreadsheetId, range: sheetName });
+    const values = await googleApi.spreadsheet.getSheets({
+      spreadsheetId: spreadsheetId,
+      ranges: ['index!A1:A10', 'index!B1:B10'],
+    });
 
-  console.log(values);
+    console.log(values.get('index!A1:A10'));
+  }
 })();
--- packages/google-api/src/spreadsheet.ts
+++ packages/google-api/src/spreadsheet.ts
@@ -1,5 +1,13 @@
-import { google } from 'googleapis';
+import { google, sheets_v4 } from 'googleapis';
 import { GoogleAuthorizer } from './authorizer';
+import { getAlphabetByColumn, RangeInfo } from './util';
+
+export interface CellInfo {
+  cell: sheets_v4.Schema$CellData;
+  width: number;
+  height: number;
+  visible: boolean;
+}
 
 export class GoogleSpreadsheetAccessor {
   private readonly authorizer: GoogleAuthorizer;
@@ -27,6 +35,69 @@ export class GoogleSpreadsheetAccessor {
 
       result.set(key, item.values || []);
     });
+
+    return result;
+  }
+
+  public async getSheets(params: { spreadsheetId: string; ranges?: string[] }) {
+    const response = await google.sheets('v4').spreadsheets.get({
+      auth: this.authorizer.authorize(),
+      spreadsheetId: params.spreadsheetId,
+      ranges: params.ranges,
+      includeGridData: true,
+    });
+
+    const requestRangeInfos = params.ranges?.map((item) => new RangeInfo(item)) || [];
+    const result = new Map<string, CellInfo[][]>();
+
+    response.data.sheets?.forEach((sheet) => {
+      const sheetName = sheet.properties?.title || '';
+
+      sheet.data?.forEach((grid) => {
+        const startColumn = getAlphabetByColumn(grid.startColumn || 0);
+        const startRow = grid.startRow || 0;
+
+        const maxLength =
+          grid.rowData
+            ?.map((row) => row.values?.length || 0)
+            .sort((a, b) => (a > b ? -1 : 1))
+            .shift() || 0;
+
+        const endColumn = getAlphabetByColumn((grid.startColumn || 0) + (maxLength - 1));
+        const endRow = grid.rowData?.length || 0;
+
+        const rangeInfo = new RangeInfo(`${sheetName}!${startColumn}${startRow + 1}:${endColumn}${endRow + 1}`);
+        const requestedInfo = requestRangeInfos
+          .sort((a, b) => (a.calcDistance(rangeInfo) < b.calcDistance(rangeInfo) ? -1 : 1))
+          .shift();
+
+        const key = requestedInfo ? requestedInfo.range : sheetName;
+        const columnMetadatas = grid.columnMetadata || [];
+        const rowMetadatas = grid.rowMetadata || [];
+
+        const data: CellInfo[][] = (grid.rowData || []).map((row, rowIndex) => {
+          const rowMetadata = rowMetadatas.length ? rowMetadatas[rowIndex] : {};
+
+          return (row.values || []).map((value, valueIndex): CellInfo => {
+            const colMetadata = columnMetadatas.length ? columnMetadatas[valueIndex] : {};
+
+            return {
+              cell: value,
+              width: colMetadata.pixelSize || 100,
+              height: rowMetadata.pixelSize || 20,
+              visible: !(
+                colMetadata.hiddenByFilter ||
+                colMetadata.hiddenByUser ||
+                rowMetadata.hiddenByFilter ||
+                rowMetadata.hiddenByUser
+              ),
+            };
+          }); // (row.values || []).map
+        }); // (grid.rowData || []).map
+
+        result.set(key, data);
+      }); // sheet.data?.forEach
+    }); // response.data.sheets?.forEach
 
     return result;
   }
