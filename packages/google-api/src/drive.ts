c6b44fef908da525b29e82b668addfe6e86cb93d
genta <code.mizumoto@gmail.com>
2023-09-17 12:36:49 +0900

    GoogleDriveにフォルダを作成する

--- packages/google-api/src/drive.ts
+++ packages/google-api/src/drive.ts
@@ -1,7 +1,7 @@
 import { google } from 'googleapis';
 import { GoogleAuthorizer } from './authorizer';
 import { createReadStream } from 'fs';
-import { basename, resolve } from 'path';
+import { basename, resolve, sep } from 'path';
 
 export class GoogleDriveAccessor {
   private readonly authorizer: GoogleAuthorizer;
@@ -21,5 +21,29 @@ export class GoogleDriveAccessor {
     });
 
     return response.data;
+  }
+
+  public async mkdir(param: { folderId?: string; folderPath: string }) {
+    const dirs = param.folderPath.split(sep);
+
+    let rootInfo: { id: string; name: string } | undefined = undefined;
+    let folderId = param.folderId;
+    for (const d of dirs) {
+      const response = await google.drive('v3').files.create({
+        auth: this.authorizer.authorize(),
+        requestBody: {
+          parents: folderId ? [folderId] : undefined,
+          name: d,
+          mimeType: 'application/vnd.google-apps.folder',
+        },
+      });
+
+      folderId = response.data.id || undefined;
+      if (!rootInfo) {
+        rootInfo = { id: folderId || '', name: d };
+      }
+    }
+
+    return rootInfo;
   }
 }
