f0f9b1e47e4fba41278d2194c521042787f9821d
genta <code.mizumoto@gmail.com>
2023-12-03 10:00:51 +0900

    fix sample

--- packages/google-api/sample/install-google-auth.ts
+++ packages/google-api/sample/install-google-auth.ts
@@ -1,7 +1,7 @@
 import { GoogleAPI } from '../src';
 
 // [usage]
-// npx ts-node sample/get-oauth2-token.ts {path_to_client_secret_json}
+// npx ts-node sample/install-google-auth.ts {key_file_json}
 
 (async () => {
   const googleApi = new GoogleAPI('mznpm-sample');
--- packages/google-api/sample/mkdir-drive.ts
+++ packages/google-api/sample/mkdir-drive.ts
@@ -1,13 +1,13 @@
 import { GoogleAPI } from '../src';
 
 // [usage]
-// npx ts-node sample/mkdir-drive.ts {path_of_folders}
+// npx ts-node sample/mkdir-drive.ts {parent_folder_id} {path_of_folders}
 
 (async () => {
   const googleApi = new GoogleAPI('mznpm-sample');
   const res = await googleApi.drive.mkdir({
-    folderId: '1kPE-1tPPiU8LSSaL35gpb8_1AMU0O0pt',
-    folderPath: process.argv[2],
+    folderId: process.argv[2],
+    folderPath: process.argv[3],
   });
 
   console.log(res);
